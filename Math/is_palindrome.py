baf56b74ca1313b1586d0e90d6fd6030993db1b2
Sarika774048 <sarikanmks9880@gmail.com>
2025-09-30 10:57:54 +0530

    Create is_palindrome.py

---
+++ Math/is_palindrome.py
@@ -0,0 +1,14 @@
+# Leetcode 9 : Palindrome Number
+#Given an integer x, return true if x is a palindrome, and false otherwise.
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            return str(x) == str(x)[::-1]
