d1ce5847eb23348f608332b5e7f6e37d20323aec
Sarika774048 <sarikanmks9880@gmail.com>
2025-10-17 22:37:29 +0530

    Create rotatedArr2.py

---
+++ Arrays/rotatedArr2.py
@@ -0,0 +1,44 @@
+# LeetCode 81 - Search in Rotated Array
+
+# There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+# Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+# Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+# You must decrease the overall operation steps as much as possible.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        left = 0
+        right = len(nums) -1
+        while left <= right:
+            mid = (left + right) //2
+            if nums[mid] == target:
+                return True            
+            while left < mid and nums[left] == nums[mid]:
+                left +=1
+            # left part
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid -1
+                else:
+                    left = mid +1
+            # right part
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid +1
+                else:
+                    right = mid -1
+
+        return False
+
+        
+            
+
+
+
+
+            
+            
+        
