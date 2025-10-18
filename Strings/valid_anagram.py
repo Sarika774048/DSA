d3f295213494c4ac7a3e9c2ff28e2d463b93db15
Sarika774048 <sarikanmks9880@gmail.com>
2025-10-18 16:25:42 +0530

    Create valid_anagram.py

---
+++ Strings/valid_anagram.py
@@ -0,0 +1,17 @@
+# LeetCode 242 - Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return Counter(s) == Counter(t)
