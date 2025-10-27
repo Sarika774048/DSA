b2bb90be3717c75c045fbed3ee30102534754b93
Sarika774048 <sarikanmks9880@gmail.com>
2025-10-27 22:59:49 +0530

    Create ransom_note.py

---
+++ HashTable/ransom_note.py
@@ -0,0 +1,17 @@
+# LeetCode 383 - Ransom Note
+
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+# Each letter in magazine can only be used once in ransomNote.
+
+# Example 1:
+
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for ch in ransomNote:
+            if ch not in magazine:
+                return False
+            magazine = magazine.replace(ch, '', 1)
+        return True
