32a5450a5a928b717a47ef32bfc6f53aee6d1e32
Sarika774048 <sarikanmks9880@gmail.com>
2025-10-31 19:29:56 +0530

    Create count_primes.py

---
+++ Math/count_primes.py
@@ -0,0 +1,22 @@
+# LeetCode 204 - Count Primes
+
+# Given an integer n, return the number of prime numbers that are strictly less than n.
+  
+# Example 1:
+
+# Input: n = 10
+# Output: 4
+# Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2 : 
+            return 0
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False
+
+        for i in range(2, int(n ** 0.5) + 1):
+            if is_prime[i]:
+                for j in range(i*i, n, i):
+                    is_prime[j] = False
+        return sum(is_prime)
