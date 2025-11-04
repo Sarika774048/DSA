31fae45aeedcc7c62be835650311c95cce9015e5
Sarika774048 <sarikanmks9880@gmail.com>
2025-11-04 21:08:54 +0530

    Create isSubtree.py

---
+++ BinaryTree/isSubtree.py
@@ -0,0 +1,35 @@
+LeetCode 572 - Subtree of Another Tree
+
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+Example 1:
+
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSameTree(s, t):
+            if not s and not t:
+                return True
+            if not s or not t:
+                return False
+            if s.val != t.val:
+                return False
+            return isSameTree(s.left, t.left) and isSameTree(s.right, t.right)
+
+        if not root:
+            return False
+        if not subRoot:
+            return True
+        if isSameTree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)        
