2468dedfb4198ab88f5e0d08c5f7fce19824a002
Sarika774048 <sarikanmks9880@gmail.com>
2025-11-06 23:47:30 +0530

    Create bottom_left_val.py

---
+++ BinaryTree/bottom_left_val.py
@@ -0,0 +1,32 @@
+LeetCode 513 - Find Bottom Left Tree Value
+
+Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+Example 1:
+
+Input: root = [2,1,3]
+Output: 1
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        leftmost = root.val
+
+        while q:
+            level_size = len(q)
+            leftmost = q[0].val
+            for _ in range(level_size):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return leftmost
+
+        
